74ed47b491b30791b0157194160ad21c8abc3ff4
Chris Green <greenc@fnal.gov>
2025-04-16 15:55:58 -0500

    New recipe: f1-config-demonstration

---
+++ packages/f1-config-demonstration/package.py
@@ -0,0 +1,34 @@
+# Copyright Spack Project Developers. See COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack.package import *
+from spack.pkg.fnal_art.fnal_github_package import *
+
+
+class F1ConfigDemonstration(CMakePackage, FnalGithubPackage):
+    """A demonstration of multi-language configuration handling for Phlex."""
+
+    homepage = "https://github.com/Framework-R-D/dune-framework-design/"
+    repo = "Framework-R-D/dune-framework-design"
+
+    git_sparse_paths=["f1_config_demonstration"]
+
+    maintainers("greenc-FNAL", "knoepfel")
+
+    license("Apache-2.0", checked_by="greenc-FNAL")
+
+    version("develop", branch="main", get_full_repo=True)
+
+    depends_on("cxx", type="build")
+
+    depends_on("py-fhicl-py@4.04:4 cxxstd=20")
+    depends_on("fhicl-cpp@4.19:4 cxxstd=20")
+    depends_on("jsonnet@0.21:")
+    depends_on("python@3.9:3")
+
+    conflicts("%gcc@:13")
+
+    @cmake_preset
+    def cmake_args(self):
+        return []
